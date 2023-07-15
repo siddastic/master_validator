545195b2dececb1483b9ee0fd92a7f255f726e7a
siddastic <siddhantgaurav2003@gmail.com>
2023-07-16 02:48:05 +0530

    remove unused imports from main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:master_validator/master_validator.dart';
 import 'package:master_validator_example/widgets/primary_button.dart';
 
