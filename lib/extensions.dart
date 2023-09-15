a21c6139608ee7bff0cc285af4cbf8c9b7c9dd7a
siddastic <siddhantgaurav2003@gmail.com>
2023-09-15 16:36:33 +0530

    add validation extensions on string

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -1,1 +1,33 @@
+import 'package:master_validator/validators.dart';
 
+extension ValidStringExt on String {
+  bool isEmail() {
+    return Validators.Required(next: Validators.Email())!(this) == null;
+  }
+
+  bool get isInteger {
+    return Validators.Required(
+            next: Validators.Number(
+                integerOnly: true, allowDecimal: false))!(this) ==
+        null;
+  }
+
+  bool get isDouble {
+    return Validators.Required(
+            next: Validators.Number(
+                integerOnly: false, allowDecimal: true))!(this) ==
+        null;
+  }
+
+  bool hasLengthBetween({int min = 0, int max = 1}) {
+    return Validators.LengthBetween(min, max)!(this) == null;
+  }
+
+  bool get isValidDirectoryName {
+    return Validators.Required(next: Validators.DirectoryName())!(this) == null;
+  }
+
+  bool get isValidURL {
+    return Validators.Required(next: Validators.Url())!(this) == null;
+  }
+}
